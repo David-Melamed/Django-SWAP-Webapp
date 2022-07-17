b72473d1728d7673f5c1de09125968852cac7f72
David-Melamed <davidmelamed269@gmail.com>
2022-07-17 18:24:36 +0300

    deployment scripts  v0.1

--- Deployment/dockercomposedown.sh
+++ Deployment/dockercomposedown.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 docker-compose -f ~/SWAP-CodeDeploy/SWAP/django_swap/docker-compose.yml down
--- Deployment/dockercomposeup.sh
+++ Deployment/dockercomposeup.sh
@@ -1,1 +1,2 @@
-docker-compose -f ~/SWAP-CodeDeploy/SWAP/django_swap/docker-compose.yml up
+#!/usr/bin/env bash
+docker-compose -f ~/SWAP-CodeDeploy/SWAP/django_swap/docker-compose.yml up -d
