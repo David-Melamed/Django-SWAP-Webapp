97c28f97177c3ecc21586708097b00f34b1153aa
David-Melamed <davidmelamed269@gmail.com>
2022-07-18 17:09:01 +0300

    fix database creation issue v0.1

--- Deployment/after_install.sh
+++ Deployment/after_install.sh
@@ -3,7 +3,7 @@ echo "AfterInstall step is starting"
 
 if docker info >/dev/null 2>&1; then
     echo "Application is running - shutting down application containers"
-    /usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml kill
+    /usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml down -v
     exit 0
 fi
 
