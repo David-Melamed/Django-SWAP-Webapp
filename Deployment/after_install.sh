a5dfacdc19926c086d1af95ee2e7bb3448ccbf2c
David-Melamed <davidmelamed269@gmail.com>
2022-07-18 15:57:27 +0300

    appspec.yml v0.4-fix

--- Deployment/after_install.sh
+++ Deployment/after_install.sh
@@ -2,7 +2,7 @@
 echo "AfterInstall step is starting"
 
 if docker info >/dev/null 2>&1; then
-    echo "Application is running - ready to shut down"
+    echo "Application is running - shutting down application containers"
     /usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml kill
     exit 1
 fi
