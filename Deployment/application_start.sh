0f9632d106d52aaa203d64c7800a1a52582e6d6f
David-Melamed <davidmelamed269@gmail.com>
2022-07-18 16:16:03 +0300

    appspec.yml v0.4-fix, sleep commands removal

--- Deployment/application_start.sh
+++ Deployment/application_start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 echo "ApplicationStart step is starting"
 
-echo "Docker-compose build"
-/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml build
-
-echo "Docker-compose up"
+echo "Docker-compose build and up"
+/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml build &&
 /usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml -d up
+
--- Deployment/validate_service.sh
+++ Deployment/validate_service.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "ValidateService step is starting"
-/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml -d up
+#/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml -d up
