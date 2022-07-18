42d5bdb058e4715ca181b02defd9f0efe8904707
David-Melamed <davidmelamed269@gmail.com>
2022-07-18 16:23:49 +0300

    appspec.yml v0.4-fix

--- Deployment/application_start.sh
+++ Deployment/application_start.sh
@@ -3,5 +3,5 @@ echo "ApplicationStart step is starting"
 
 echo "Docker-compose build and up"
 /usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml build &&
-/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml -d up
+/usr/local/bin/docker-compose -f /var/code/SWAP-Web/SWAP/django_swap/docker-compose.yml up -d
 
