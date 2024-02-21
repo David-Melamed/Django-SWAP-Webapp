11c3c1247ef9dc33ceabd9835cb8d0b4f5a01675
David-Melamed <davidmelamed269@gmail.com>
2024-02-21 16:49:32 +0200

    attach policies to iam role

--- Deployment/EBS/modules/iam_role/iam_role.tf
+++ Deployment/EBS/modules/iam_role/iam_role.tf
@@ -6,3 +6,15 @@ resource "aws_iam_role" "ebslab_role" {
   name = var.role_name
   assume_role_policy = jsonencode(local.assume_role_policy)
 }
+
+# Attach AWS Elastic Beanstalk policy
+resource "aws_iam_role_policy_attachment" "ebslab_beanstalk_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkFullAccess"
+  role       = aws_iam_role.ebslab_role.name
+}
+
+# Attach AWS EC2 policy
+resource "aws_iam_role_policy_attachment" "ebslab_ec2_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role       = aws_iam_role.ebslab_role.name
+}
