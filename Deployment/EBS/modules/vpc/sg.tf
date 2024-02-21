e936ff235c860c0db92707e1880d4588d78ed574
David-Melamed <davidmelamed269@gmail.com>
2024-02-21 17:30:17 +0200

    add ec2 ssh support

--- Deployment/EBS/modules/vpc/sg.tf
+++ Deployment/EBS/modules/vpc/sg.tf
@@ -3,15 +3,23 @@ resource "aws_security_group" "ebslab_security_group" {
   name        = "ebslab-sg"
   description = "Security group for Elastic Beanstalk"
 
-  // Assuming you have a VPC defined, replace "vpc-id" with your actual VPC ID
   vpc_id      = aws_vpc.ebslab_vpc.id
 }
 
-resource "aws_security_group_rule" "ebslab_security_group_rule" {
+resource "aws_security_group_rule" "ebslab_security_group_rule_8080" {
   security_group_id = aws_security_group.ebslab_security_group.id
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic from any IP address
+}
+
+resource "aws_security_group_rule" "ebslab_security_group_rule_ssh" {
+  security_group_id = aws_security_group.ebslab_security_group.id
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # Allow SSH from any IP address
 }
