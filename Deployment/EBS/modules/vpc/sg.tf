651c71468f8358519f09244a8d1dc3f3b2a296d8
David-Melamed <davidmelamed269@gmail.com>
2024-02-21 16:51:06 +0200

    add sg

---
+++ Deployment/EBS/modules/vpc/sg.tf
@@ -0,0 +1,17 @@
+# Create a security group
+resource "aws_security_group" "ebslab_security_group" {
+  name        = "ebslab-sg"
+  description = "Security group for Elastic Beanstalk"
+
+  // Assuming you have a VPC defined, replace "vpc-id" with your actual VPC ID
+  vpc_id      = aws_vpc.ebslab_vpc.id
+}
+
+resource "aws_security_group_rule" "ebslab_security_group_rule" {
+  security_group_id = aws_security_group.ebslab_security_group.id
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]  # Allow traffic from any IP address
+}
