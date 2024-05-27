80056ff5ef86f41a8ab8d758b8ec2a172fa5e7aa
David-Melamed <davidmelamed269@gmail.com>
2024-05-27 20:07:34 +0300

    allow internal traffic on port 80

--- Deployment/EBS/modules/vpc/sg.tf
+++ Deployment/EBS/modules/vpc/sg.tf
@@ -1,9 +1,17 @@
 # Create a security group
 resource "aws_security_group" "ebslab_security_group" {
+  vpc_id      = aws_vpc.ebslab_vpc.id
   name        = var.sg_name
   description = "Security group for Elastic Beanstalk"
+}
 
-  vpc_id      = aws_vpc.ebslab_vpc.id
+resource "aws_security_group_rule" "ebslab_security_group_rule_80" {
+  security_group_id = aws_security_group.ebslab_security_group.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.ebslab_security_group.id
 }
 
 resource "aws_security_group_rule" "ebslab_security_group_rule_ssh" {
