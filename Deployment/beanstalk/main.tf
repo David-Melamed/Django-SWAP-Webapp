f360b442edb9fd1651dd63e1e77fb34e8701eda4
David-Melamed <davidmelamed269@gmail.com>
2024-05-29 13:12:09 +0300

    Place inherited and dynamic values in subsequent sections

--- Deployment/beanstalk/main.tf
+++ Deployment/beanstalk/main.tf
@@ -6,10 +6,10 @@ module "vpc" {
   vpc_cidr                = "10.0.0.0/16"
   public_sn_count         = 2
   public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
-  map_public_ip_on_launch = false
   rt_route_cidr_block     = "0.0.0.0/0"
   sg_name                 = "swapapp-sg"
   enable_dns_hostnames    = true
+  map_public_ip_on_launch = false
 }
 
 module "iam" {
@@ -21,68 +21,69 @@ module "iam" {
 }
 
 module "rds" {
-  source                  = "./modules/rds"
-  allocated_storage       = 10
-  engine                  = "mysql"
-  engine_version          = 5.7
-  instance_class          = "db.t3.micro"
-  username                = "root"
-  password                = "password"
-  db_name                 = "my_application"
-  skip_final_snapshot     = true
-  subnet_name             = module.vpc.sg_name
-  subnet_ids              = module.vpc.subnet_ids
-  vpc_security_group_id   = module.vpc.security_group_id
-  vpc_id                  = module.vpc.vpc_id
-  instance_private_ips    = module.beanstalk.instance_private_ips
-  private_subnet_ids      = module.vpc.private_subnet_ids
-  public_subnet_ids       = module.vpc.public_subnet_ids
-  }
+  source                = "./modules/rds"
+  allocated_storage     = 10
+  engine                = "mysql"
+  engine_version        = 5.7
+  instance_class        = "db.t3.micro"
+  username              = "root"
+  password              = "password"
+  db_name               = "my_application"
+  skip_final_snapshot   = true
+  subnet_name           = module.vpc.sg_name
+  subnet_ids            = module.vpc.subnet_ids
+  vpc_security_group_id = module.vpc.security_group_id
+  vpc_id                = module.vpc.vpc_id
+  instance_private_ips  = module.beanstalk.instance_private_ips
+  private_subnet_ids    = module.vpc.private_subnet_ids
+  public_subnet_ids     = module.vpc.public_subnet_ids
+}
 
 module "route53_zone" {
-  source                  = "./modules/route53/zone"
-  zone_name               = "swapapp.net"
-  vpc_id                  = module.vpc.vpc_id
+  source    = "./modules/route53/zone"
+  zone_name = "swapapp.net"
+  vpc_id    = module.vpc.vpc_id
 }
 
 module "route53_rds_record" {
-  source                  = "./modules/route53/rds_record"
-  zone_name               = module.route53_zone.zone_name
-  rds_record_name         = join("-", ["rds", "dev"])
-  rds_address             = module.rds.db_endpoint
-  zone_id                 = module.route53_zone.zone_id
+  source          = "./modules/route53/rds_record"
+  rds_record_name = join("-", ["rds", "dev"])
+  zone_name       = module.route53_zone.zone_name
+  rds_address     = module.rds.db_endpoint
+  zone_id         = module.route53_zone.zone_id
 }
 
 module "route53_registered_domains" {
-  source                  = "./modules/route53/registered_domains"
-  zone_name               = module.route53_zone.zone_name
-  zone_id                 = module.route53_zone.zone_id
-  zone_web_name_servers   = module.route53_zone.name_servers
+  source                = "./modules/route53/registered_domains"
+  zone_name             = module.route53_zone.zone_name
+  zone_id               = module.route53_zone.zone_id
+  zone_web_name_servers = module.route53_zone.name_servers
 }
 
 module "acm" {
-  source                  = "./modules/acm"
-  domain_name             = module.route53_zone.zone_name
-  zone_id                 = module.route53_zone.zone_id
+  source      = "./modules/acm"
+  domain_name = module.route53_zone.zone_name
+  zone_id     = module.route53_zone.zone_id
 }
 
 module "beanstalk" {
-  source                  = "./modules/beanstalk"
-  ebs_app_name            = "swapapp-web"
-  ebs_app_description     = "Python Web App Application using Django Framework" 
-  solution_stack_name     = "64bit Amazon Linux 2023 v4.3.2 running Docker"
-  env                     = "dev"
-  service_role_name       = "aws-elasticbeanstalk-ec2-role"
-  service_role_arn        = module.iam.role_arn
-  vpc_id                  = module.vpc.vpc_id
-  instance_type           = "t3.small"
-  security_group_id       = module.vpc.security_group_id
-  bucket_name             = join("-", [module.beanstalk.ebs_app_name, "bucket"])
-  application_version     = "v1.94"
-  ssl_certificate_arn     = module.acm.ssl_certificate_arn
+  source                    = "./modules/beanstalk"
+  ebs_app_name              = "swapapp-web"
+  ebs_app_description       = "Python Web App Application using Django Framework"
+  solution_stack_name       = "64bit Amazon Linux 2023 v4.3.2 running Docker"
+  env                       = "dev"
+  service_role_name         = "aws-elasticbeanstalk-ec2-role"
+  instance_type             = "t3.small"
+  bucket_name               = join("-", [module.beanstalk.ebs_app_name, "bucket"])
+  application_version       = "v1.94"
+  service_role_arn          = module.iam.role_arn
+  vpc_id                    = module.vpc.vpc_id
+  ssl_certificate_arn       = module.acm.ssl_certificate_arn
   subnet_availability_zones = module.vpc.subnet_availability_zones
-  zone_id                 = module.route53_zone.zone_id
-  zone_name               = module.route53_zone.zone_name
-  private_subnet_ids      = module.vpc.private_subnet_ids
-  public_subnet_ids       = module.vpc.public_subnet_ids
+  zone_id                   = module.route53_zone.zone_id
+  zone_name                 = module.route53_zone.zone_name
+  private_subnet_ids        = module.vpc.private_subnet_ids
+  public_subnet_ids         = module.vpc.public_subnet_ids
+  security_group_id         = module.vpc.security_group_id
+
 }
