629ffb45c70a239e32991a5b27b5bcd7a6dce2b2
David-Melamed <davidmelamed269@gmail.com>
2024-06-05 22:36:57 +0300

    export ebs settings to fetch auto scaling instance ips

--- Deployment/beanstalk/modules/beanstalk/outputs.tf
+++ Deployment/beanstalk/modules/beanstalk/outputs.tf
@@ -38,3 +38,7 @@ output "application_version" {
 output "bucket_name" {
   value = var.bucket_name
 }
+
+output "ebs_all_settings" {
+  value = aws_elastic_beanstalk_environment.ebslab_env.all_settings
+}
