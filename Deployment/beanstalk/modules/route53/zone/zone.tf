b8098dbd01c3dceb152d3f3762c11eaead378d23
David-Melamed <davidmelamed269@gmail.com>
2024-06-05 17:16:11 +0300

    reduced NS TTL to 1m

--- Deployment/beanstalk/modules/route53/zone/zone.tf
+++ Deployment/beanstalk/modules/route53/zone/zone.tf
@@ -5,3 +5,19 @@ resource "aws_route53_zone" "ebs-lab" {
     Environment = "dev"
   }
 }
+
+
+resource "aws_route53_record" "ns_record" {
+  allow_overwrite = true
+  name            = var.zone_name
+  ttl             = 60
+  type            = "NS"
+  zone_id         = aws_route53_zone.ebs-lab.zone_id
+
+  records = [
+    aws_route53_zone.ebs-lab.name_servers[0],
+    aws_route53_zone.ebs-lab.name_servers[1],
+    aws_route53_zone.ebs-lab.name_servers[2],
+    aws_route53_zone.ebs-lab.name_servers[3]
+  ]
+}
