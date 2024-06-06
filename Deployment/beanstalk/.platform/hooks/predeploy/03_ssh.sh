97890f835b0babedeeb9ae926389382e76ba9dac
David-Melamed <davidmelamed269@gmail.com>
2024-06-06 15:16:55 +0300

    refactor script

--- Deployment/beanstalk/.platform/hooks/predeploy/03_ssh.sh
+++ Deployment/beanstalk/.platform/hooks/predeploy/03_ssh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Define paths for the keys
+# Define the user and paths for the keys
 USER="devops_user"
-LOCAL_KEY_PATH="$HOME/.ssh/authorized_keys"  # The authorized_keys file path
+USER_HOME="/home/$USER"
+USER_SSH_DIR="$USER_HOME/.ssh"
+LOCAL_KEY_PATH="$USER_SSH_DIR/authorized_keys"  # The authorized_keys file path for devops_user
 TEMP_KEY_PATH="/var/app/staging/.platform/ssh/id_rsa.pub"  # Temporary public key path
 
 # Check if the temporary public key exists
@@ -11,11 +13,12 @@ if [ ! -f "$TEMP_KEY_PATH" ]; then
     exit 1
 fi
 
-# Ensure the .ssh directory exists and has the right permissions
-mkdir -p "$HOME/.ssh"
-chmod 700 "$HOME/.ssh"
+# Ensure the .ssh directory exists and has the right permissions for devops_user
+sudo -u $USER mkdir -p "$USER_SSH_DIR"
+sudo chown $USER:$USER "$USER_SSH_DIR"
+sudo chmod 700 "$USER_SSH_DIR"
 
-# Ensure the authorized_keys file exists and has the right permissions
+# Ensure the authorized_keys file exists and has the right permissions for devops_user
 if [ ! -f "$LOCAL_KEY_PATH" ]; then
     sudo -u $USER touch "$LOCAL_KEY_PATH"
 fi
@@ -26,10 +29,10 @@ sudo chmod 600 "$LOCAL_KEY_PATH"
 sudo -u $USER bash -c "cat $TEMP_KEY_PATH >> $LOCAL_KEY_PATH"
 
 # Verify the key was copied successfully
-if grep -qxF -f "$TEMP_KEY_PATH" "$LOCAL_KEY_PATH"; then
+if sudo -u $USER grep -qxF -f "$TEMP_KEY_PATH" "$LOCAL_KEY_PATH"; then
     echo "SSH public key deployment completed, you are now able to connect to the server via SSH."
     # Optionally, clean up the temporary file
-    rm "$TEMP_KEY_PATH"
+    sudo rm "$TEMP_KEY_PATH"
 else
     echo "Failed to deploy the SSH public key."
     exit 1
